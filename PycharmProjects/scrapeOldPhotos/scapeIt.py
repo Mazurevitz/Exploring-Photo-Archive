c61d1b3e65ff84b9b1e5f95da5e2bc98a90a0ee9
Mazurevitz <wojtekmaz.glowny@gmail.com>
2017-11-22 04:35:20 +0100

    Initiation of project, soup the photo description

--- PycharmProjects/scrapeOldPhotos/scapeIt.py
+++ PycharmProjects/scrapeOldPhotos/scapeIt.py
@@ -1,5 +1,7 @@
 import urllib
 import urllib.request
+import re
+
 from bs4 import BeautifulSoup
 
 
@@ -9,9 +11,35 @@ def make_soup(url):
     soupdata = BeautifulSoup(thepage, "html.parser")
     return soupdata
 
+source = "https://audiovis.nac.gov.pl/"
 
-soup = make_soup("https://audiovis.nac.gov.pl/obraz/5326/h:448/")
+soup1 = make_soup(source + "haslo/448/")
 
-for img in soup.findAll("a", attrs={"opis":True}):
-    print(img)
-    # print(img.get('src'))
+for img in soup1.findAll("img"):
+    print(img.get('src'))
+
+
+# soup_list = [make_soup("https://audiovis.nac.gov.pl/obraz/" + str(i) + "/h:448/") for i in range(10)]
+# soup_list_filtered = [soup_list[i].findAll("a", attrs={"opis": True}) for i in range(len(soup_list))]
+#
+# for img in soup_list_filtered:
+#     print(img)
+
+
+# Check if value is integer
+def RepresentsInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+data = []
+soup = make_soup("https://audiovis.nac.gov.pl/haslo/448:1/")
+for img in soup.findAll("a", attrs={"href":True}):
+    if RepresentsInt(img.text):
+        data.append(img.text)
+
+print(max(data))
+last_site = max(data)
+
